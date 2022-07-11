fbbd9ee745bebe975a33cadea6b9606c64542f7c
Felicitas Pojtinger <felicitas@pojtinger.com>
2022-07-11 18:18:50 +0200

    Add test for for `Shift`

--- pkg/pool/linked_list_test.go
+++ pkg/pool/linked_list_test.go
@@ -1,7 +1,6 @@
 package pool
 
 import (
-	"log"
 	"reflect"
 	"testing"
 )
@@ -169,8 +168,6 @@ func TestDelete(t *testing.T) {
 					return false
 				}
 
-				log.Println(dll.toArray())
-
 				return reflect.DeepEqual(dll.toArray(), []string{})
 			},
 		},
@@ -266,7 +263,102 @@ func TestDelete(t *testing.T) {
 			tt.apply(list, tt.before(list))
 
 			if rv := tt.check(list); rv != true {
-				t.Errorf("check DoubleLinkedList.Insert() = %v, want true", rv)
+				t.Errorf("check DoubleLinkedList.Delete() = %v, want true", rv)
+			}
+		})
+	}
+}
+
+func TestShift(t *testing.T) {
+	tests := []struct {
+		name    string
+		before  func(*DoubleLinkedList[string])
+		shiftBy int
+		check   func(*DoubleLinkedList[string]) bool
+	}{
+		{
+			name:    "Works with no inserts",
+			before:  func(dll *DoubleLinkedList[string]) {},
+			shiftBy: 0,
+			check: func(dll *DoubleLinkedList[string]) bool {
+				if dll.Len() != 0 {
+					return false
+				}
+
+				return reflect.DeepEqual(dll.toArray(), []string{})
+			},
+		},
+		{
+			name:    "Works with no inserts and 5 shifts",
+			before:  func(dll *DoubleLinkedList[string]) {},
+			shiftBy: 5,
+			check: func(dll *DoubleLinkedList[string]) bool {
+				if dll.Len() != 0 {
+					return false
+				}
+
+				return reflect.DeepEqual(dll.toArray(), []string{})
+			},
+		},
+		{
+			name: "Works with 1 inserts and 1 shift",
+			before: func(dll *DoubleLinkedList[string]) {
+				dll.Insert("One")
+			},
+			shiftBy: 1,
+			check: func(dll *DoubleLinkedList[string]) bool {
+				if dll.Len() != 0 {
+					return false
+				}
+
+				return reflect.DeepEqual(dll.toArray(), []string{})
+			},
+		},
+		{
+			name: "Works with 3 inserts and 1 shift",
+			before: func(dll *DoubleLinkedList[string]) {
+				dll.Insert("One")
+				dll.Insert("Two")
+				dll.Insert("Three")
+			},
+			shiftBy: 1,
+			check: func(dll *DoubleLinkedList[string]) bool {
+				if dll.Len() != 2 {
+					return false
+				}
+
+				return reflect.DeepEqual(dll.toArray(), []string{"Two", "One"})
+			},
+		},
+		{
+			name: "Works with 3 inserts and 3 shifts",
+			before: func(dll *DoubleLinkedList[string]) {
+				dll.Insert("One")
+				dll.Insert("Two")
+				dll.Insert("Three")
+			},
+			shiftBy: 3,
+			check: func(dll *DoubleLinkedList[string]) bool {
+				if dll.Len() != 0 {
+					return false
+				}
+
+				return reflect.DeepEqual(dll.toArray(), []string{})
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			list := NewDoubleLinkedList[string]()
+
+			tt.before(list)
+
+			for i := 0; i < tt.shiftBy; i++ {
+				list.Shift()
+			}
+
+			if rv := tt.check(list); rv != true {
+				t.Errorf("check DoubleLinkedList.Shift() = %v, want true", rv)
 			}
 		})
 	}
